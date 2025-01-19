8f4bf6c3c8155462fadfbf0f1fdedc62a8455951
Reubencfernandes <18reuchagasfernandes@gmail.com>
2025-01-19 19:33:52 +0530

    projects section left to be done

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import Navbar from "@/components/navbar";
+import Footer from "@/components/Footer";
 import Hero from "@/components/hero";
 import About from "@/components/about";
+import Projects from "@/components/projects";
+import TechStack from "@/components/stack";
 export default function Home() {
   return (
-    <>
-    <Navbar/>
+    <div className="">
     <Hero/>
     <About/>
-    </>
+    <Projects/>
+    <TechStack/>
+    <Footer/>
+    </div>
   );
 }
---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+    return (
+        <footer className="bg-gray-800 text-white py-4">
+            <p className="text-center">&copy; 2024 Reuben Fernandes</p>
+        </footer>
+    );
+};
+
+export default Footer;
