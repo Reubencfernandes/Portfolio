6a3ddab0b973157e39caa20a56b6501d79faab84
Reubencfernandes <18reuchagasfernandes@gmail.com>
2025-01-19 18:15:34 +0530

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "@/components/navbar";
+import Hero from "@/components/hero";
+import About from "@/components/about";
 export default function Home() {
   return (
     <>
     <Navbar/>
+    <Hero/>
+    <About/>
     </>
   );
 }
